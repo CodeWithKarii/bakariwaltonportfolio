697d16b7615771985e245642db79dfa1291d9031
CodeWithKarii <bwalton10@sycamores.indstate.edu>
2023-05-30 02:27:28 -0400

    changed app.py database

--- app.py
+++ app.py
@@ -4,17 +4,6 @@ from flask_sqlalchemy  import SQLAlchemy
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'kashflow'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///usersport.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db = SQLAlchemy(app)
-
-class Users(db.Model):
-    _id = db.Column('id',db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-
 
 def __init__(self,name, email):
     self.name = name
@@ -44,5 +33,4 @@ def chatbook():
 
 
 if __name__ == '__main__':
-    db.create_all()
     app.run()
