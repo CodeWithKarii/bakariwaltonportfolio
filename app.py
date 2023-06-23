540c2ae8d505516c30b1b82cd151c0fa97ade36e
CodeWithKarii <bwalton10@sycamores.indstate.edu>
2023-06-23 11:45:35 -0400

    TIMESTAMP AND GITGUB LINK UPDATE

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, url_for ,send_from_directory
-
+from datetime import datetime 
 
 
 
@@ -7,14 +7,23 @@ app = Flask(__name__)
 
 @app.route("/", methods=['GET','POST'])
 def index():
+    # get the current date and time
+     post1 = datetime(2023, 6, 10, 12, 56, 54, 324893)
+     post2 = datetime(2023, 6, 23, 12, 56, 54, 324893)
+     date_element1 = post1.date()
+     date_element2 = post2.date()
 
-    if request.method == 'POST':
+
+     if request.method == 'POST':
         # Get the data from the POST request
         name= request.form['name']
         email = request.form['email']
         # print(name)
         # print(email)
-    return  render_template('index.html')
+        return  render_template('index.html')
+     return render_template('index.html' ,post1=date_element1, post2=date_element2)
+
+
 
 
 @app.route("/roll")
@@ -26,11 +35,13 @@ def roll():
 def chatbook():
     return  render_template('chat.html')
 
-@app.route('/favicon.ico')
-def fav():
-    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
+# @app.route('/favicon.ico')
+# def fav():
+#     return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico')
 
 
 
 if __name__ == '__main__':
+    # app.run()
+    
     app.run(host='0.0.0.0', port=80)
